4f220a9c2e7f324ee3e08749d74ef7e440c99f00
Maria Eduarda da Nóbrega <eduardanbg2005@gmail.com>
2025-01-21 18:06:49 -0300

    atualizando 2

---
+++ Operadores/operadoresAritmeticos.js
@@ -0,0 +1,15 @@
+//const salario = 5000;
+//const despesas = 1500;
+//const valeAlimentação = 500;
+
+//console.log("Saldo restante:", salario + valeAlimentação - despesas);
+
+//console.log("--------------------------");
+
+//const n1 = 10;
+//const n2 = 5;
+
+//console.log("Divisão: ", n1/n2);
+//console.log("Multiplicação: ", n1*n2);
+//console.log("Módulo da divisão: ", n1%n2);
+
---
+++ Operadores/operadoresLogicos.js
@@ -0,0 +1,6 @@
+console.log("Eduarda foi parada em uma Blitz.");
+
+const idade = 17;
+const habilitacao = true;
+
+console.log("Eduarda tem 18 anos e habilitação?", idade >= 18 && habilitacao);
